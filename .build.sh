a13c72f601e8d28b123798cb2f316f517de6b4b1
Brett <brett.whynot@gmail.com>
2024-09-12 11:26:02 +1000

    Update build

--- .build.sh
+++ .build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 cd "${0%/*}"
 OUTPUT="${1:-TA-icmp.spl}"
-pip install --upgrade --no-dependencies -t lib -r lib/requirements.txt
-rm -rf lib/splunklib/__pycache__
-rm -rf lib/splunklib/searchcommands/__pycache__
-rm -rf lib/splunklib/modularinput/__pycache__
-rm -rf lib/*/__pycache__
+chmod -R u=rwX,go= *
+chmod -R u-x+X *
+chmod -R u=rwx,go= *
+python3.9 -m pip install --upgrade -t lib -r lib/requirements.txt --no-dependencies
 cd ..
 tar -cpzf $OUTPUT --exclude=.* --exclude=package.json --overwrite TA-icmp 

5f5ea6054d750c86fbb71c30b62c67394a16d243
Brett <brett.whynot@gmail.com>
2024-11-17 09:23:24 +1000

    Remove test

--- bin/test.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-print(sys.path[0])
-print(os.path.join(sys.path[0],".."))
-print(os.path.join(sys.path[0],"..","local/targets.csv"))
-print(os.path.join(sys.path[0],"..","../TA-icmp/local/targets.csv"))
-print()
-print(os.path.join(os.path.dirname(os.path.abspath(__file__))))
-print(os.path.join(os.path.dirname(os.path.abspath(__file__)),"..","local/targets.csv"))
